21ac7e8b29235d35b634d2bffabd6ba30e17fd75
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-02-11 22:01:13 +0700

    first commit

---
+++ scrypt.js
@@ -0,0 +1,47 @@
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu')
+const kontakme = document.getElementById("kontakme");
+
+
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+
+    if(window.scrollY > fixednav) {
+        header.classList.add('navbar-fixed')
+    }else {
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+hamburger.addEventListener('click', function () { 
+    hamburger.classList.toggle('hamburger-active')
+    console.log("hamburger ditekan");
+
+    navMenu.classList.toggle('hidden')
+ })
+
+ document.getElementById("downloadCv").addEventListener("click", function() {
+    var pdfURL = "assets/my-cv.pdf";
+  
+    var link = document.createElement("a");
+    link.href = pdfURL;
+    link.download = "my-cv.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+
+  kontakme.addEventListener("click", function(event) {
+    event.preventDefault()
+    const email = "assyafiqnaufal625@gmail.com";
+    const subjek = document.getElementById("judulPesan").value;
+    const isiPesan = document.getElementById("message").value;
+    
+    const encodedSubject = encodeURIComponent(subjek);
+    const encodedBody = encodeURIComponent(isiPesan);
+
+    const gmailURL = "https://mail.google.com/mail/?view=cm&fs=1&to=" + email + "&su=" + encodedSubject + "&body=" + encodedBody;
+
+    window.open(gmailURL, '_blank');
+});
